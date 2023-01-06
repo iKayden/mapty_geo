4c43411760410caebc50b4266ee0051c11f972ce
ikayden <ikyden23@gmail.com>
2023-01-05 23:36:35 -0800

    Add three classes

--- script.js
+++ script.js
@@ -77,5 +77,49 @@ class App {
     form.classList.add("hidden");
   }
 }
+
+class Workout {
+  // ES6 class fields
+  date = new Date();
+  id = (Date.now() + "").slice(-10); // Bad Idea, but works for now
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace(); // runs on creation of the instance
+  };
+
+  calcPace() {
+    // min per km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevGain) {
+    super(coords, distance, duration);
+    this.elevGain = elevGain;
+    this.calcSpeed();
+  };
+
+  calcSpeed() {
+    // kilometers per hour = kilometers / (hours + (minutes / 60))
+    this.speed = this.duration / (this.distance / 60);
+    return this.speed;
+  }
+
+}
 // Initializing the App class
 const app = new App();
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycle1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycle1);
