a474346a45034c70d5eb964f492ca9c6a36338f2
ikayden <ikyden23@gmail.com>
2023-01-05 21:35:29 -0800

    add form

--- script.js
+++ script.js
@@ -13,16 +13,40 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition((position) => {
     const { latitude, longitude } = position.coords;
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView(coords, 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    L.marker([51.505, -0.09])
-      .addTo(map)
-      .bindPopup("pop up lol")
-      .openPopup();
+
+
+    // part of Leaflet library (on())
+    // add markers on the map where user clicked
+    map.on("click", function(mapEvent) {
+      form.classList.remove("hidden");
+      inputDistance.focus();
+
+    });
 
   }, (error) => {
     alert("Could not get your position");
   });
+
+form.addEventListener("submit", function(e) {
+  // Display marker after form is filled and submitted
+  const { lat, lng } = mapEvent.latlng;
+  const coords = [lat, lng];
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup(L.popup({
+      maxWidth: 250,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+      className: "running-popup"
+    }))
+    .setPopupContent("Workout")
+    .openPopup();
+});
